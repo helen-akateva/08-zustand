4283a407a499b7fd4d88f94a76dcc2fc7d2596b0
helen-akateva <akareva17@gmail.com>
2025-10-28 17:06:02 +0200

    add meta and font

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,9 +5,37 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import NoteDetails from "./NoteDetails.client";
+import { Metadata } from "next";
 
 interface Props {
   params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+
+  const note = await getNoteById(id);
+
+  const title = `Note ${note.title}`;
+  const description = `Read and manage your note titled "${note.title}". Stay organized with NoteHub.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `http://localhost:3000/notes/${id}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: `NoteHub note ${id} preview`,
+        },
+      ],
+    },
+  };
 }
 
 export default async function NoteDetailsPage({ params }: Props) {
